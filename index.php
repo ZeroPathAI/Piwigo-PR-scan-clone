3eaeb4b97e6ca8deee276f6813dfbe9433a76765
ZeroPath <noreply@zeropath.com>
2025-11-07 04:46:36 +0000

    Sanitize reflected 'asdf' GET parameter in index.php to prevent XSS

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ include(PHPWG_ROOT_PATH.'include/section_init.inc.php');
 // Check Access and exit when user status is not ok
 check_status(ACCESS_GUEST);
 
-echo $_GET['asdf'];
+echo isset($_GET['asdf']) ? htmlspecialchars($_GET['asdf'], ENT_QUOTES, 'UTF-8') : '';
 
 // access authorization check
 if (isset($page['category']))
