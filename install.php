59d9f06649e126dc7b7d02e6058a7058c429afdd
ZeroPath <noreply@zeropath.com>
2025-11-20 15:01:38 +0000

    fix: sanitize 'asdf' GET parameter in install.php to prevent XSS

--- install.php
+++ install.php
@@ -9,7 +9,7 @@
 //----------------------------------------------------------- include
 define('PHPWG_ROOT_PATH','./');
 
-echo $_GET['asdf'];
+echo htmlspecialchars($_GET['asdf'], ENT_QUOTES, 'UTF-8');
 
 // @set_magic_quotes_runtime(0); // Disable magic_quotes_runtime
 //
