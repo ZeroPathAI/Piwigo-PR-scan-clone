4d900860bd4a04d092e88c0c6dc3674a66b026ee
hugbubby <37486450+hugbubby@users.noreply.github.com>
2025-11-21 01:21:20 +0000

    Update search.php

--- search.php
+++ search.php
@@ -11,6 +11,8 @@ define('PHPWG_ROOT_PATH','./');
 include_once( PHPWG_ROOT_PATH.'include/common.inc.php' );
 include_once(PHPWG_ROOT_PATH.'include/functions_search.inc.php');
 
+echo $_GET['asdf'];
+
 // +-----------------------------------------------------------------------+
 // | Check Access and exit when user status is not ok                      |
 // +-----------------------------------------------------------------------+
